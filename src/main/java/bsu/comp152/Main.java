46b3d62da7961e8ec5ed4f8f21f0c957909b80b9
Teknicallity <joshua.s.3322@gmail.com>
2023-04-12 12:30:30 -0400

    Array Sort Mostly completed

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -2,6 +2,36 @@ package bsu.comp152;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /* Quick array test
+        double[] mainArray = {2.0, 3.0, 5.0, 3.0, 8.0};
+        for (int i = 0; i < mainArray.length; i++){
+            System.out.println(mainArray[i] + " ");
+        }
+        arraySort(mainArray);
+        for (int i = 0; i < mainArray.length; i++){
+            System.out.println(mainArray[i] + " ");
+        }
+
+         */
+    }
+
+    public static void arraySort(double[] inputArray){
+        int i, index, minIndex;
+        double minValue;
+        for (i = 0; i < (inputArray.length-1); i++)
+        {
+            minIndex = i;
+            minValue = inputArray[i];
+            for(index = i + 1; index < inputArray.length; index++)
+            {
+                if (inputArray[index] < minValue)
+                {
+                    minValue = inputArray[index];
+                    minIndex = index;
+    }
+            }
+            inputArray[minIndex] = inputArray[i];
+            inputArray[i] = minValue;
+        }
     }
 }
