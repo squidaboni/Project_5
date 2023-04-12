77dc131b4995275e80b449071aec3e6a8b7fb082
Teknicallity <joshua.s.3322@gmail.com>
2023-04-12 12:55:15 -0400

    Array Sort functionally completed

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -16,20 +16,27 @@ public class Main {
          */
     }
 
+    /**
+     * Method written by Josh Sheputa, Bridgewater State, jsheputa@student.bridgew.edu, 4/12/23
+     * Takes a double array, then sequentially sorts them from smallest to largest value
+     * @param inputArray given array to sort
+     *
+     */
     public static void arraySort(double[] inputArray){
+        //integer iterable, 2nd iterable, whole index progress
         int i, index, minIndex;
+        //minimum value for minIndex to end of array
         double minValue;
-        for (i = 0; i < (inputArray.length-1); i++)
-        {
+        //for every item in array, and for every item within the specified bounds
+        for (i = 0; i < (inputArray.length-1); i++){
             minIndex = i;
             minValue = inputArray[i];
-            for(index = i + 1; index < inputArray.length; index++)
-            {
-                if (inputArray[index] < minValue)
-                {
+            for(index = i + 1; index < inputArray.length; index++){
+                //check if index value is lesser than current minimum, set as new minimum if true
+                if (inputArray[index] < minValue) {
                     minValue = inputArray[index];
                     minIndex = index;
-    }
+                }
             }
             inputArray[minIndex] = inputArray[i];
             inputArray[i] = minValue;
